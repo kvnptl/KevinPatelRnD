bb7fbbdd6f9a825a968a2414dbe91d6f57da1811
kvnptl <kevinpatel4400@gmail.com>
2023-11-04 23:09:05 +0100

    [Logs]

--- bash_scripts/mt_detr/test/mt_detr/run_test_MT-DETR_c+l+r_model.sh
+++ bash_scripts/mt_detr/test/mt_detr/run_test_MT-DETR_c+l+r_model.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks-per-node=64    # cores
 #SBATCH --mem 180GB               # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --time 0-24:00              # total runtime of job allocation (format D-HH:MM)
-#SBATCH --output test_hrfuser_TINY_nus_r640_l_r_fusion_output.%j.out # filename for STDOUT (%N: nodename, %j: job-ID)
-#SBATCH --error test_hrfuser_TINY_nus_r640_l_r_fusion_output.%j.err  # filename for STDERR
+#SBATCH --output test_mt_detr_c+l+r_output.%j.out # filename for STDOUT (%N: nodename, %j: job-ID)
+#SBATCH --error test_mt_detr_c+l+r_output.%j.err  # filename for STDERR
 
 echo "[bash] My HOSTNAME is "
 echo `hostname`
@@ -21,25 +21,33 @@ echo "[bash] Loading GCC and CUDA modules..."
 module load gcc/10.1.0 cuda/10.2
 
 . "/home/kpatel2s/anaconda3/etc/profile.d/conda.sh"
-conda activate hrfuser-cuda102-torch110-mmcv-full-1317
+conda activate mt_detr
 
-cd /home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser
+cd /home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/mt_detr
 
 echo "[bash] Directory changed to $(pwd)"
 
-echo "[bash] Start testing HRFuser TINY model on nuScenes dataset..."
+echo "[bash] Start testing MT-DETR Camera + Lidar + Radar model on DENSE dataset..."
 
 echo -e "[bash] --------------------------------------------\n"
 
 #############
 ### NOTE: change checkpoint path accordingly
 #############
-python tools/test.py configs/hrfuser/cascade_rcnn_hrfuser_t_1x_nus_r640_l_r_fusion_orig.py /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/hrfuser_weights/nuscenes/work_dirs/hrfuser_TINY_nuScenes_c_l_r_fusion_epoch_12_batch_12_orig_config_multi_gpu_2023-11-02_00-28-39_214445/epoch_12.pth \
-        --work-dir /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/hrfuser_weights/nuscenes/inference/hrfuser_TINY_nuScenes_c_l_r_fusion_orig_config_multi_gpu_2023-11-02_00-28-39_214445${CURRENT_DATE_TIME}_${SLURM_JOB_ID} \
+weather_list=(test_clear_day test_clear_night light_fog_day light_fog_night dense_fog_day dense_fog_night snow_day snow_night)
+
+for w in "${weather_list[@]}"
+do
+    python tools/test.py \
+        configs/mt_detr/mt_detr_c+l+r.py \
+        /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/mt_detr_weights/work_dirs/camera_lidar_radar_single_multi_single_A100_gpu_2023-11-03_08-31-53_214469/epoch_36.pth \
+        --weather ${w} \
+        --work-dir /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/mt_detr_weights/inference/camera_lidar_radar_single_multi_single_A100_gpu_2023-11-03_08-31-53_214469_${CURRENT_DATE_TIME}_${SLURM_JOB_ID} \
         --eval bbox \
         --show \
-        --show-dir /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/hrfuser_weights/nuscenes/inference/hrfuser_TINY_nuScenes_c_l_r_fusion_orig_config_multi_gpu_2023-11-02_00-28-39_214445${CURRENT_DATE_TIME}_${SLURM_JOB_ID} \
-        --cfg-options data.test.samples_per_gpu=32
+        --show-dir /home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/model_weights/mt_detr_weights/inference/camera_lidar_radar_single_multi_single_A100_gpu_2023-11-03_08-31-53_214469_${CURRENT_DATE_TIME}_${SLURM_JOB_ID} \
+        --cfg-options data.test.samples_per_gpu=16
+done
 
 echo "[bash] Testing completed..."
 
