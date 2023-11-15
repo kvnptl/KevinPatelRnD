27f1c23eae9a3acedd7dc54f0a4e92f479450432
kvnptl <kevinpatel4400@gmail.com>
2023-11-15 09:45:58 +0100

    [utils] json file

--- scripts/analyze_json.py
+++ scripts/analyze_json.py
@@ -17,9 +17,61 @@ def modify_json_file(json_data):
     with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/mt_detr/data/coco_annotation/dense_fog_night_modified_2.json', 'w') as f:
         json.dump(json_data, f)
 
+def modify_json_based_on_id(json_data, id=0):
+    """
+    Only keep dict whose id is 4
+    """
+    modified_data = {
+        'images': [],
+        'lidar_projections': [],
+        'radar_projections': [],
+        'annotations': [],
+        'categories': json_data['categories']
+    }
+    query = None
+    for data in json_data:
+        if data == 'annotations':
+            query = 'image_id'
+        elif data == 'categories':
+            continue
+        else:
+            query = 'id'
+        for d in json_data[data]:
+            if d[query] == id:
+                modified_data[data].append(d)
+
+    # Write the modified data back to the file
+    with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser_cuda11p1/data/dense/dense_infos_all_modified_coco_2_only_4.json', 'w') as f:
+        json.dump(modified_data, f)
+
+def modify_json_based_on_id_mt_detr(json_data, id=0):
+    """
+    Only keep dict whose id is 4
+    """
+    modified_data = {
+        'images': [],
+        'annotations': [],
+        'categories': json_data['categories']
+    }
+    query = None
+    for data in json_data:
+        if data == 'annotations':
+            query = 'image_id'
+        elif data == 'categories':
+            continue
+        else:
+            query = 'id'
+        for d in json_data[data]:
+            if d[query] == id:
+                modified_data[data].append(d)
+
+    # Write the modified data back to the file
+    with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/mt_detr/data/coco_annotation/dense_fog_night_modified_only_3.json', 'w') as f:
+        json.dump(modified_data, f)
+
 
 def main():
-    json_file_path = '/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/mt_detr/data/coco_annotation/dense_fog_night_modified.json'  # Replace with your JSON file path
+    json_file_path = '/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser_cuda11p1/data/dense/dense_infos_all_modified_coco_2.json'  # Replace with your JSON file path
     
     with open(json_file_path, 'r') as f:
         json_data = json.load(f)
@@ -32,7 +84,10 @@ def main():
     
     print(f"The total number of keys in the JSON file is {total_keys}.")
 
-    modify_json_file(json_data)
+    # modify_json_file(json_data)
+
+    modify_json_based_on_id(json_data, id=4)
+    # modify_json_based_on_id_mt_detr(json_data, id=3)
 
 if __name__ == '__main__':
     main()
