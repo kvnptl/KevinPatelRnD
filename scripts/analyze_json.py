a91a852db7a3011b8252dae1b9940e948db499bf
kvnptl <kevinpatel4400@gmail.com>
2023-11-20 02:23:55 +0100

    [utils]

--- scripts/analyze_json.py
+++ scripts/analyze_json.py
@@ -10,11 +10,21 @@ def count_keys_in_dict(d):
     return count
 
 def modify_json_file(json_data):
-    json_data['images'] = json_data['images'][:5]
-    json_data['annotations'] = json_data['annotations'][:23]
+    json_data['images'] = json_data['images'][:2]
+    json_data['annotations'] = json_data['annotations'][:19]
 
     # save the modified data back to a new JSON file
-    with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/mt_detr/data/coco_annotation/dense_fog_night_modified_2.json', 'w') as f:
+    with open('/home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/datasets/nuScenes/v1.0-trainval/gt_fcos_coco_val_modified_2.json', 'w') as f:
+        json.dump(json_data, f)
+
+def modify_json_file_hrfuser_nuscenes(json_data):
+    json_data['images'] = json_data['images'][:2]
+    json_data['annotations'] = json_data['annotations'][:10]
+    json_data['lidar_projections'] = json_data['lidar_projections'][:2]
+    json_data['radar_projections'] = json_data['radar_projections'][:2]
+
+    # save the modified data back to a new JSON file
+    with open('/home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/datasets/nuScenes/nuscenes_infos_val_mono3d_modified_2.coco.json', 'w') as f:
         json.dump(json_data, f)
 
 def modify_json_based_on_id(json_data, id=0):
@@ -71,7 +81,8 @@ def modify_json_based_on_id_mt_detr(json_data, id=0):
 
 
 def main():
-    json_file_path = '/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser_cuda11p1/data/dense/dense_infos_all_modified_coco_2.json'  # Replace with your JSON file path
+    # json_file_path = '/home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/datasets/nuScenes/v1.0-trainval/gt_fcos_coco_test.json'  # Replace with your JSON file path
+    json_file_path = '/home/kpatel2s/kpatel2s/link_scratch_dir/kpatel2s/datasets/nuScenes/nuscenes_infos_val_mono3d_saf_fcos.coco.json'
     
     with open(json_file_path, 'r') as f:
         json_data = json.load(f)
@@ -80,19 +91,34 @@ def main():
         print("The JSON file does not contain a dictionary (JSON object) at the top level.")
         return
 
-    check_images = True
     # Print total number of images
     if 'images' in json_data:
         total_images = len(json_data['images'])
         print(f"The total number of images in the JSON file is {total_images}.")
+
+        # For SAF-FCOS to HRFuser comparison
+
+        # From HRfuset to SAF fcos
+        # img_name = 'n015-2018-08-02-17-16-37+0800__CAM_FRONT__1533201470412460.jpg'
+        # for image in json_data['images']:
+        #     if img_name in image['file_name']:
+        #         print(image['file_name'])
+        #         breakpoint()
+
+        # From SAF fcos to hrfuser comparison
+        # img_name = 'n008-2018-08-01-16-03-27-0400__CAM_FRONT__1533153857912404'
+        # for image in json_data['images']:
+        #     if 'n008-2018-08-01-16-03-27-0400__CAM_FRONT__1533153857912404' in image['file_name']:
+        #         print(f'Found image: {image["file_name"]}')
 
     total_keys = count_keys_in_dict(json_data)
     
     print(f"The total number of keys in the JSON file is {total_keys}.")
 
     # modify_json_file(json_data)
+    modify_json_file_hrfuser_nuscenes(json_data)
 
-    modify_json_based_on_id(json_data, id=4)
+    # modify_json_based_on_id(json_data, id=4)
     # modify_json_based_on_id_mt_detr(json_data, id=3)
 
 if __name__ == '__main__':
