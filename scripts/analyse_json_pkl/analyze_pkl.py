4eac9105131be0bf42039d7910e288975783ffcb
kvnptl <kevinpatel4400@gmail.com>
2023-12-05 07:37:27 +0100

    [utils] Replace 'Van' class with 'Car' in dense_infos_all.pkl

--- scripts/analyse_json_pkl/analyze_pkl.py
+++ scripts/analyse_json_pkl/analyze_pkl.py
@@ -1,7 +1,7 @@
 import pickle
 
 # Replace 'your_file.pkl' with the path to your .pkl file
-with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser/data/dense/dense_infos_all.pkl', 'rb') as file:
+with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser_cuda11p1/data/dense/dense_infos_all_modified_van_as_car.pkl', 'rb') as file:
     data = pickle.load(file)
 
 # # remove some dict
@@ -11,20 +11,37 @@ with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser/data/
 # with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser/data/dense/dense_infos_dense_fog_modified.pkl', 'wb') as file:
 #     pickle.dump(modified_data, file)
 
-pass
-
+print(f'Total number of data: {len(data)}') 
 # Pick the same ids as MT-DETR
+
+# data_modified = []
+# # extract same image is in pkl file as mt-detr
+# for d in data:
+#     if d['image']['image_idx'] == '2018-02-07_18-39-52_00300' or d['image']['image_idx'] == '2018-12-12_15-21-22_00300' or d['image']['image_idx'] == '2018-12-12_15-21-22_02700' or d['image']['image_idx'] == '2018-12-12_15-18-40_00400' or d['image']['image_idx'] == '2018-12-14_15-27-11_04300':
+#         # save the d and store to create a new pkl
+#         data_modified.append(d)
+#         print(f"found {d['image']['image_idx']}")
+
+# Replace 'Van' to 'Car' class
+a = input("Are you sure you want to replace 'Van' to 'Car'? (sure/not): ")
+
+if a != 'sure':
+    exit()
 
 data_modified = []
 # extract same image is in pkl file as mt-detr
 for d in data:
     if d['image']['image_idx'] == '2018-02-07_18-39-52_00300' or d['image']['image_idx'] == '2018-12-12_15-21-22_00300' or d['image']['image_idx'] == '2018-12-12_15-21-22_02700' or d['image']['image_idx'] == '2018-12-12_15-18-40_00400' or d['image']['image_idx'] == '2018-12-14_15-27-11_04300':
+        for i, class_names in enumerate(d['annos']['name']):
+            if class_names == 'Van':
+                d['annos']['name'][i] = 'Car'
+        
         # save the d and store to create a new pkl
         data_modified.append(d)
         print(f"found {d['image']['image_idx']}")
 
 # Write the modified data back to the file
-with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser/data/dense/dense_infos_modified.pkl', 'wb') as file:
+with open('/home/kpatel2s/kpatel2s/sensor_fusion_rnd/KevinPatelRnD/hrfuser_cuda11p1/data/dense/dense_infos_all_modified_van_as_car.pkl', 'wb') as file:
     pickle.dump(data_modified, file)
 
 # MT-DETR image ids
